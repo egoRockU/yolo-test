123242673c6a15faec954ea9f464ad7f2bcd7209
egoRockU <lagutan.gerwin.bscs2021@gmail.com>
2025-02-06 15:32:59 +0800

    updated pi_server

--- pi_server.py
+++ pi_server.py
@@ -8,46 +8,65 @@ import cv2
 import numpy as np
 from ultralytics import YOLO
 from picamera2 import Picamera2
-from gpiozero import LED
 
 # INITIAL SETUPS
-SERIAL = "/dev/ttyACM0"
+VENDO_SERIAL = "/dev/ttyACM0"
+FILTER_SERIAL = "/dev/ttyUSB0"
+
 app = Flask(__name__)
-ser = Serial()
-ser.baudrate = 115200
-ser.port = SERIAL
-ser.timeout = 1
+
+# VENDO SERIAL CONNECTION
+vendo_ser = Serial()
+vendo_ser.baudrate = 115200
+vendo_ser.port = VENDO_SERIAL
+vendo_ser.timeout = 1
+
+# FILTER SERIAL CONNECTION
+filter_ser = Serial()
+filter_ser.baudrate = 115200
+filter_ser.port = FILTER_SERIAL
+filter_ser.timeout = 1
 
 data_buffer = []
 buffer_lock = Lock()
-flash = LED(23)
 
 
 @app.route('/')
 def hello_world():
-   with buffer_lock:
-       print (data_buffer)
+    return render_template('home.html')
 
-   return data_buffer
+@app.route('/serial-send-filter', methods=['POST'])
+def serial_send_filter():
+    data = request.form
+    message = data['message']
+    print(message)
+    res = "res: " + message + "\n"
+    if filter_ser.is_open:
+        print("Sent: " + message)
+        filter_ser.write(res.encode())
+    else: 
+        print("Sending Failed")
+    return render_template('home.html')
 
 
-def serial_listen():
+def vendo_serial_listen():
     time.sleep(5)
     try:
-        ser.open()
+        vendo_ser.open()
+        filter_ser.open()
     except Exception as e:
         print(f"Failed to open serial port: {e}")
         return
    
     while True:
-        if ser.in_waiting:
-            data = ser.readline().decode('utf8').strip()
+        if vendo_ser.in_waiting:
+            data = vendo_ser.readline().decode('utf8').strip()
             if 'OBJECT DETECTED!' in data:
                 image_arr = pi_capture_image()
                 if image_arr is None:
                     data_buffer.append('Camera did not work properly')
                     res = "res: " + str(4) + "\n"
-                    ser.write(res.encode())
+                    vendo_ser.write(res.encode())
                     print(data_buffer)
                 else:
                     bottle_class = identify_bottle(image_arr)
@@ -57,15 +76,21 @@ def serial_listen():
                             data_buffer.append("Not Bottle")
                             res = "res: " + str(3) + "\n"
                             print(res)
-                            ser.write(res.encode())
+                            vendo_ser.write(res.encode())
                             print(data_buffer)
                     else:
                         print("bottle class: ", bottle_class)
                         with buffer_lock:
                             data_buffer.append(bottle_class)
                             res = "res: " + str(bottle_class) + "\n"
-                            ser.write(res.encode())
+                            vendo_ser.write(res.encode())
                             print(data_buffer)
+            
+            if 'TOTAL LITERS:' in data:
+                liters = int(data.split(':')[-1].strip())
+                res = "res: " + str(liters) + "\n"
+                filter_ser.write(res.encode())
+                data_buffer = []
 
 
 def capture_image():
@@ -95,16 +120,14 @@ def pi_capture_image():
     picam2.configure(camera_config)
 
     picam2.start()
-    flash.on()
     time.sleep(2)
     frame = picam2.capture_array()
-    flash.off()
     picam2.close()
     
     return frame
 
 def identify_bottle(image_array):
-    yolo_model = "models/300ep.pt"
+    yolo_model = "models/1.3kSetAug.pt"
     cls_list = ["Large", "Medium", "Small"]
 
     model = YOLO(yolo_model)
@@ -123,7 +146,7 @@ def identify_bottle(image_array):
 
 if __name__ == '__main__':
     # Start the serial listener thread
-    listener_thread = threading.Thread(target=serial_listen, daemon=True)
+    listener_thread = threading.Thread(target=vendo_serial_listen, daemon=True)
     listener_thread.start()
 
     # Run the Flask app
